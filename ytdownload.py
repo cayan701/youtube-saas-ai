663a8b232abfe17b9fed78c76febe43dee4b0f1c
cayan701 <cayan701@gmail.com>
2025-07-18 13:47:42 +0530

    YT init

--- ytdownload.py
+++ ytdownload.py
@@ -1,2 +1,11 @@
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
+
+
 url1 = "https://youtu.be/motX94ztOzo?si=3TQ28uCzzn9-Q9Q-"
 url2 = "https://www.youtube.com/watch?v=motX94ztOzo&si=3TQ28uC"
+
+yt = YouTube(url1, on_progress_callback=on_progress)
+print(yt.title)
+
+ys = yt.streams.get_highest_resolution()
